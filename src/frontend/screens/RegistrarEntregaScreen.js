ce395d43e21a8b2cdde4b267f2145b6784c27248
sabrynavn <Sabrynanoronhav@gmail.com>
2026-07-08 22:57:49 -0300

    feat: captura localização GPS logo após a foto

--- src/frontend/screens/RegistrarEntregaScreen.js
+++ src/frontend/screens/RegistrarEntregaScreen.js
@@ -1,4 +1,5 @@
 import {CameraView, useCameraPermissions} from 'expo-camera';
+import * as Location from 'expo-location';
 import { useRef, useState } from 'react';
 import {View,Text,Button , Image} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -7,6 +8,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
     const [permissao , solicitarPermissao] = useCameraPermissions();
      const cameraRef = useRef(null);
      const [fotoUri, setFotoUri] = useState(null);
+     const [localizacao, setLocalizacao] = useState(null);
 
 
     //Enquanto o hook ainda não terminou de consultar o sistema operacional
@@ -35,14 +37,44 @@ import {SafeAreaView} from 'react-native-safe-area-context';
     if (!cameraRef.current) return;
     const foto = await cameraRef.current.takePictureAsync();
     setFotoUri(foto.uri);
+
+    // Pede permissão de localização (se ainda não tiver)
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permissão de localização negada');
+      return;
+    }
+
+    // Captura a posição ATUAL (não uma posição em cache/antiga)
+    const posicao = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+
+    setLocalizacao({
+      latitude: posicao.coords.latitude,
+      longitude: posicao.coords.longitude,
+      timestamp: posicao.timestamp,
+    });
   }
+  
 
   // Se já existe uma foto tirada, mostramos a prévia dela
   if (fotoUri) {
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <Image source={{ uri: fotoUri }} style={{ flex: 1 }} />
-        <Button title="Tirar outra foto" onPress={() => setFotoUri(null)} />
+        {localizacao && (
+             <Text>
+            Lat: {localizacao.latitude} | Long: {localizacao.longitude}
+          </Text>
+        )}
+
+        <Button title="Tirar outra foto"
+         onPress={() => {
+            setFotoUri(null)
+            setLocalizacao(null)
+        }}
+         />
       </SafeAreaView>
     );
   }
